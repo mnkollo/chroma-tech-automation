2a5a2a341e84864d3151d417fd3f7a65e5213e20
mnkollo <73963114+mnkollo@users.noreply.github.com>
2024-03-12 08:35:03 -0500

    finished section 10

--- src/test/java/com/chromatech/automation/Locators.java
+++ src/test/java/com/chromatech/automation/Locators.java
@@ -16,17 +16,41 @@ public class Locators {
         String url = "https://chroma.mexil.it/site/login";
         driver.get(url);
 
-        // Locating using id attribute
+        // Locating using id locator/method
         WebElement userNameTextBox = driver.findElement(By.id("form-username"));
         userNameTextBox.sendKeys("general@teacher.com");
 
-        // Locating using name attribute
+        // Locating using name locator/method
         // WebElement passwordTextBox = driver.findElement(By.name("password"));
         // passwordTextBox.sendKeys("123456");
 
-        // Locating using class name method
-        WebElement passwordTextBox = driver.findElement(By.className("form-password"));
-        passwordTextBox.sendKeys("123456");
+        // Locating using class name locator/method
+        // WebElement passwordTextBox = driver.findElement(By.className("form-password"));                   // Compound class names are not allowed
+        // passwordTextBox.sendKeys("123456");
+
+        // Locating using tagName locator/method
+        //driver.findElement(By.tagName("input")).sendKeys("123456");
+
+        // Locating using linkText locator/method
+        // driver.findElement(By.linkText("Forgot Password?")).click();                                      // must enter the entire text of the link
+
+        // Locating using partialLinkText locator/method
+        //(driver.findElement(By.partialLinkText("Forgot Pass"))).click();                     // you can enter a part of the text of the link, or entire text and it will work
+
+        // Locating using cssSelector locator/method #attributeValueOfID
+        // driver.findElement(By.cssSelector("#form-password")).sendKeys("123456");
+
+        // Locating using cssSelector locator/method [attribute='value']
+        // driver.findElement(By.cssSelector("[class='form-password form-control']")).sendKeys("123456");
+
+        // // Locating using full/absolute xpath locator/method
+        // driver.findElement(By.xpath("/html/body/div/div/div/div/div/div/div/div[2]/form/div[2]/input")).sendKeys("123456");                 //not recommended
+
+        // Locating using relative xpath locator/method
+        driver.findElement(By.xpath("//*[@class='form-password form-control']")).sendKeys("123456");
+
+        // Locating using relative xpath Text locator/method
+        driver.findElement(By.xpath("//*[contains(text(),'Sign In')]")).click();;
 
     }
 }
