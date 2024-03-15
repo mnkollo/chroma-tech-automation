97adb13c77042ce7e96eb59689daedae73f5631b
mnkollo <73963114+mnkollo@users.noreply.github.com>
2024-03-14 20:24:55 -0500

    finished CheckBoxes

--- src/test/java/com/chromatech/automation/Checkboxes.java
+++ src/test/java/com/chromatech/automation/Checkboxes.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class RadioButtons {
+public class Checkboxes {
 
     public static void main(String[] args) {
         
@@ -18,11 +18,13 @@ public class RadioButtons {
 
 
         // by xpath
-        driver.findElement(By.xpath("//input[@name='radioButton']")).click();
+        driver.findElement(By.xpath("//input[@value='option1']")).click();
+        // driver.findElement(By.xpath("//input[@value='option2']")).click();
+        // driver.findElement(By.xpath("//input[@value='option3']")).click();
 
-        // index
-        driver.findElement(By.xpath("(//input[@name='radioButton'])[2]")).click();
 
+        // by index
+        driver.findElement(By.xpath("(//input[@type='checkbox'])[6]")).click();
     }
     
 }
