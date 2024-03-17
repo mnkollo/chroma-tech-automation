bef2dbd8c41ea357e913586cf1eaa72c2befe316
mnkollo <73963114+mnkollo@users.noreply.github.com>
2024-03-17 16:44:02 -0500

    finished Alerts

---
+++ src/test/java/com/chromatech/automation/Alerts.java
@@ -0,0 +1,44 @@
+package com.chromatech.automation;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alerts {
+
+    public static void main(String [] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+
+        String url = "https://chroma-tech-academy.mexil.it/static_page/";
+        driver.get(url);
+
+        WebElement alertTextBox = driver.findElement(By.cssSelector("[id='name']"));
+        alertTextBox.sendKeys("Michael");
+
+        WebElement alertButton = driver.findElement(By.cssSelector("fieldset [id='alertbtn']"));
+        alertButton.click();
+        Thread.sleep(2000);
+
+
+        //clicking on button that opens alert with 'ok' button
+        
+        // Getting the text from the alert
+        Alert alert = driver.switchTo().alert();
+
+        String alertText = alert.getText();
+        System.out.println(alertText);  
+
+        //alert.accept();  //Used to confirm a positive scenario  (so click on the ok button)
+        alert.dismiss(); //Used to confirm a negative scenario (so click on the cancel button)
+
+        Thread.sleep(2000);
+        driver.quit();
+  }
+    
+}
