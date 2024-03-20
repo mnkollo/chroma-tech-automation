e33e0ae38e493a4df349b47a6328060c3041ddf0
mnkollo <73963114+mnkollo@users.noreply.github.com>
2024-03-19 21:56:20 -0500

    finished before and after methods

--- src/test/java/com/chromatech/automation/Ebay_Sanity.java
+++ src/test/java/com/chromatech/automation/Ebay_Sanity.java
@@ -1,35 +1,54 @@
 package com.chromatech.automation;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test; // Add import for Test annotation
 import org.testng.asserts.SoftAssert;
 import org.testng.Assert;
 
 public class Ebay_Sanity {
-   
-    @Test
-    public void empty_Search_test() throws InterruptedException{
+    WebDriver driver;{    // Add WebDriver driver; to the class level, Declare the driver
 
-        
-        WebDriver driver = new ChromeDriver();
+    }; 
+
+    @BeforeClass
+    public void setUpClass() {
+        System.out.println("Before CLass Settings Done");
+        System.out.println("Execution Starts: EBayHome_Sanity");
+    }
+
+    @AfterClass
+    public void tearDownClass() {
+        System.out.println("After Class Settngs Done");
+        System.out.println("Execution Ends: EBayHome_Sanity");
+    }
+
+    @BeforeMethod
+    public void setUp() throws InterruptedException {
+        driver = new ChromeDriver();        // Add this line to initialize the driver,
         driver.manage().window().maximize();
         driver.get("https://www.ebay.com/");
         Thread.sleep(2000);
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void empty_Search_test() throws InterruptedException {
+
         String expectedURL = "https://www.ebay.com/n/all-categories?_from=R40&_trksid=p4432023.m570.l1313&_nkw=&_sacat=0";
         String expectedTitle = "Shop by Category | eBay";
-        
-        //Assert.assertTrue(driver.findElement(By.cssSelector("[id='gh-btn']")).isEnabled(),"Verify Search button is enabled");
-        //Assert.assertFalse(driver.findElement(By.cssSelector("[id='gh-btn']")).isSelected(),"Verify Search button is not selected");
-
-        Assert.assertNull(expectedTitle, "Verify Title of the page");
-        Assert.assertNotNull(expectedURL, "Verify URL of the page");
-
 
         driver.findElement(By.cssSelector("[id='gh-btn']")).click();
         String newUrl = driver.getCurrentUrl();
@@ -38,42 +57,34 @@ public class Ebay_Sanity {
         System.out.println("The new URL is: " + newUrl);
         System.out.println("The new Title is: " + newTitle);
 
-        Assert.assertEquals(newUrl,expectedURL, "Verify URL of the new page");
-        Assert.assertEquals(newTitle,expectedTitle, "Verify Title of the new page");
+        Assert.assertEquals(newUrl, expectedURL, "Verify URL of the new page");
+        Assert.assertEquals(newTitle, expectedTitle, "Verify Title of the new page");
 
-        //Assert.assertNotEquals(newUrl, expectedURL);
-
-        driver.quit();
     }
 
     @Test
-    public void empty_Search_test_softassert() throws InterruptedException{
+    public void empty_Search_test_softassert() throws InterruptedException {
 
         SoftAssert sa = new SoftAssert();
 
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.ebay.com/");
-        Thread.sleep(2000);
         String expectedURL = "https://www.ebay.com/n/all-categories?_from=R40&_trksid=p4432023.m570.l1313&_nkw=&_sacat=0";
         String expectedTitle = "Electronics, Cars, Fashion, Collectibles & More | eBay";
-        
-        sa.assertTrue(driver.findElement(By.cssSelector("[id='gh-btn']")).isEnabled(),"Verify Search button is enabled");
 
-        
+        sa.assertTrue(driver.findElement(By.cssSelector("[id='gh-btn']")).isEnabled(),
+                "Verify Search button is enabled");
+
         Thread.sleep(2000);
         String newUrl = driver.getCurrentUrl();
         String newTitle = driver.getTitle();
         System.out.println("The new URL is: " + newUrl);
         System.out.println("The new Title is: " + newTitle);
 
-        //sa.assertEquals(newUrl," ", "Verify URL of the new page");
-        sa.assertEquals(newTitle,expectedTitle, "Verify Title of the new page");
+        sa.assertEquals(newUrl,expectedTitle, "Verify URL of the new page");
+        sa.assertEquals(newTitle, expectedTitle, "Verify Title of the new page");
 
         WebElement searchTxtBox = driver.findElement(By.cssSelector("[class='gh-tb ui-autocomplete-input']"));
         searchTxtBox.sendKeys("Hello There");
         driver.findElement(By.cssSelector("[id='gh-btn']")).click();
-        driver.quit();
 
         sa.assertAll();
     }
